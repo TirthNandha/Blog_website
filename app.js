756ff50e65345789f1dd71b078bf302512530810
TirthNandha <ntirth15@gmail.com>
2024-03-14 16:24:42 +0530

    connected cloud db

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require('lodash');
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,7 +15,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://0.0.0.0:27017/BlogDB")
+mongoose.connect(process.env.DB_URI)
 
 const postSchema = new mongoose.Schema({
   title: String,
@@ -25,7 +26,6 @@ const Post = mongoose.model("Post", postSchema)
 
 app.get("/", async function (req, res) {
   const BlogsArray = await Post.find()
-  console.log(BlogsArray)
   res.render("home", { firstPara: homeStartingContent, postsArray: BlogsArray })
 
 })
@@ -42,12 +42,11 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 
-app.get("/posts/:postName", function (req, res) {
-  posts.forEach(function (post) {
-    if (_.lowerCase(post.title) === _.lowerCase(req.params.postName)) {
-      res.render("post", { postTitle: post.title, postContent: post.content })
-    }
-  })
+app.get("/posts/:postId", async function (req, res) {
+  const requestedPostId = req.params.postId;
+  const foundedPost = await Post.findOne({_id: requestedPostId})
+  res.render("post", { postTitle: foundedPost.title, postContent: foundedPost.content })
+  
 })
 
 app.get("/about", function (req, res) {
